0db377e89a9dd54c85db0c837684c0f1fe9a7b56
sanjithpk <sanjithpk06@gmail.com>
2021-05-26 08:33:08 +0530

    added the styled components and created the restaurant card info component

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar"
 import React from "react"
-import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native"
+import { ThemeProvider } from "styled-components/native"
+
+import { theme } from "./src/infrastructure/theme"
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen"
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   )
 }
-
-const styles = StyleSheet.create({})
